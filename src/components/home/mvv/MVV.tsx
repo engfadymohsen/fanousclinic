ba819fa899304e3cdae0e76a9589c6546f937312
engfadymohsen <144414724+engfadymohsen@users.noreply.github.com>
2023-12-04 09:14:01 +0200

    Mission & Vision With The Same Height

--- src/components/home/mvv/MVV.tsx
+++ src/components/home/mvv/MVV.tsx
@@ -5,84 +5,57 @@ import RemoveRedEyeRoundedIcon from '@mui/icons-material/RemoveRedEyeRounded';
 import AdjustRoundedIcon from '@mui/icons-material/AdjustRounded';
 import theme from '../../../theme/theme';
 
-
-
-
-
-
-
-
-
 const MVV = () => {
-    return (
-      <Box sx={{
-          width: '95%',
-          margin: '0 auto',
-          padding: '1rem 0',
-          boxSizing: 'border-box',
-      }}>
-          <Grid container spacing={2}>
-              <Grid item xs={12} sm={6} md={6}>
-                  <Box sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      boxShadow: '0px 0px 6px #0334492f',
-                      padding: '20px',
-                      borderRadius: '15px',
-                      minHeight: '100%', // Set to 100% to match the height of the sibling
-                  }}>
-                      <AdjustRoundedIcon sx={{color: theme.primary_color, fontSize: '10rem'}} />
-                      <Typography sx={{fontSize: '2rem', fontWeight: '700'}}>Mission</Typography>
-                      <Box sx={{width: '50%', height: '2px', backgroundColor: theme.sec_color}}></Box>
-                      <Typography sx={{
-                          textAlign: 'justify', 
-                          color: theme.primary_color, 
-                          marginTop: '2.5rem', 
-                          fontSize: theme.body_font, 
-                          hyphens: 'auto'
-                      }}>
-                          We empower our patients with personalized healthcare that is efficient and effective.
-                          We help them achieve their health goals and live their best lives.
-                      </Typography>
-                  </Box>
-              </Grid>
-              <Grid item xs={12} sm={6} md={6}>
-                  <Box sx={{
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center',
-                      boxShadow: '0px 0px 6px #0334492f',
-                      padding: '20px',
-                      borderRadius: '15px',
-                      minHeight: '100%', // Set to 100% to match the height of the sibling
-                  }}>
-                      <RemoveRedEyeRoundedIcon sx={{color: theme.primary_color, fontSize: '10rem'}} />
-                      <Typography sx={{fontSize: '2rem', fontWeight: '700'}}>Our Vision</Typography>
-                      <Box sx={{width: '50%', height: '2px', backgroundColor: theme.sec_color}}></Box>
-                      <Typography sx={{
-                          textAlign: 'justify', 
-                          color: theme.primary_color, 
-                          marginTop: '2.5rem', 
-                          fontSize: theme.body_font, 
-                          hyphens: 'auto'
-                      }}>
-                          To be the world's largest enterprise for mass customization of healthcare.
-                      </Typography>
-                  </Box>
-              </Grid>           
-          </Grid>
-      </Box>
-    )
-  }
-  
+  return (
+    <Box sx={{
+        width: '95%',
+        margin: '0 auto',
+        padding: '1rem 0',
+        boxSizing: 'border-box',
+        display: 'flex', // Make this a flex container
+        flexDirection: 'column', // Column direction to manage vertical spacing
+        height: '100%', // Full height
+    }}>
+        <Grid container spacing={2} sx={{ flex: 1 }}> {/* Flex 1 to fill the parent container */}
+            <Grid item xs={12} sm={6} md={6} sx={{ display: 'flex' }}>
+                <Box sx={{
+                    flex: 1, // Flex 1 to fill the grid item container
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    boxShadow: '0px 0px 6px #0334492f',
+                    padding: '20px',
+                    borderRadius: '15px',
+                }}>
+                    <AdjustRoundedIcon sx={{ color: theme.primary_color, fontSize: '10rem' }} />
+                    <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>Mission</Typography>
+                    <Box sx={{ width: '50%', height: '2px', backgroundColor: theme.sec_color }}></Box>
+                    <Typography sx={{ textAlign: 'justify', color: theme.primary_color, marginTop: '2.5rem', fontSize: theme.body_font, hyphens: 'auto' }}>
+                        We empower our patients with personalized healthcare that is efficient and effective. We help them achieve their health goals and live their best lives.
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={6} sx={{ display: 'flex' }}>
+                <Box sx={{
+                    flex: 1, // Flex 1 to fill the grid item container
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    boxShadow: '0px 0px 6px #0334492f',
+                    padding: '20px',
+                    borderRadius: '15px',
+                }}>
+                    <RemoveRedEyeRoundedIcon sx={{ color: theme.primary_color, fontSize: '10rem' }} />
+                    <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>Our Vision</Typography>
+                    <Box sx={{ width: '50%', height: '2px', backgroundColor: theme.sec_color }}></Box>
+                    <Typography sx={{ textAlign: 'justify', color: theme.primary_color, marginTop: '2.5rem', fontSize: theme.body_font, hyphens: 'auto' }}>
+                        To be the world’s largest enterprise for mass customization of healthcare.
+                    </Typography>
+                </Box>
+            </Grid>           
+        </Grid>
+    </Box>
+  )
+}
 
-
-
-
-
-
-
-
-
-export default MVV
+export default MVV;
