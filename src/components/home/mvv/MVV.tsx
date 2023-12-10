0bb41d68a5d1608151cebf0259d2f41021773a3f
engfadymohsen <82896079+fadymohsen@users.noreply.github.com>
2023-12-10 07:22:44 +0200

    Update MVV.tsx

--- src/components/home/mvv/MVV.tsx
+++ src/components/home/mvv/MVV.tsx
@@ -31,7 +31,8 @@ const MVV = () => {
                     <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>Mission</Typography>
                     <Box sx={{ width: '50%', height: '2px', backgroundColor: theme.sec_color }}></Box>
                     <Typography sx={{ textAlign: 'center', color: theme.primary_color, marginTop: '2.5rem', fontSize: theme.body_font}}>
-                        We empower our patients with personalized healthcare that is efficient and effective. We help them achieve their health goals and live their best lives.
+                        We empower patients with personalized healthcare that is efficient and effective.
+                        We help them to achieve their health goals by discovering the truth of wellness.
                     </Typography>
                 </Box>
             </Grid>
@@ -49,7 +50,7 @@ const MVV = () => {
                     <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>Our Vision</Typography>
                     <Box sx={{ width: '50%', height: '2px', backgroundColor: theme.sec_color }}></Box>
                     <Typography sx={{ textAlign: 'center', color: theme.primary_color, marginTop: '2.5rem', fontSize: theme.body_font, hyphens: 'auto' }}>
-                        To be the world’s largest enterprise for mass customization of healthcare.
+                        “To Become the Most Recognized Healthcare company For Helping every person on earth to Realize the Way to a Healthy Life.”
                     </Typography>
                 </Box>
             </Grid>           
