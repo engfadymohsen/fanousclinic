219f84e55c858f6d08ed6fe7591e4814db77603d
engfadymohsen <82896079+fadymohsen@users.noreply.github.com>
2023-12-12 06:59:19 +0200

    Update MVV.tsx

--- src/components/home/mvv/MVV.tsx
+++ src/components/home/mvv/MVV.tsx
@@ -50,7 +50,7 @@ const MVV = () => {
                     <Typography sx={{ fontSize: '2rem', fontWeight: '700' }}>Our Vision</Typography>
                     <Box sx={{ width: '50%', height: '2px', backgroundColor: theme.sec_color }}></Box>
                     <Typography sx={{ textAlign: 'center', color: theme.primary_color, marginTop: '2.5rem', fontSize: theme.body_font}}>
-                        To Become the Most Recognized Healthcare company on Earth by Nurturing every person 
+                        To Become the Most Recognized Healthcare Company on Earth by Nurturing Every Person 
                         to Realize The Way to a Healthy Life.
                     </Typography>
                 </Box>
